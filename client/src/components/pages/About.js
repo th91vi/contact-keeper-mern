f1192b603e88ffac1374eb079cc9dd6d0ffeb080
Thiago Vieira <th91v@outlook.com>
2020-07-18 19:51:11 -0300

    Limpeza do CRA. Adição do componente Navbar. Adição das páginas home e About

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this app</h1>
+            <p>
+                This is a full stack React app for keeping contacts. Part of the <strong>React Front to Back</strong> course.
+            </p>
+        </div>
+    )
+}
+
+export default About
