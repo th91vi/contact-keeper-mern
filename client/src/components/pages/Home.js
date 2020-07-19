4d3bb753781691c415964242f6e54dbb1624c890
Thiago Vieira <th91v@outlook.com>
2020-07-19 11:06:14 -0300

    Substituição de CSS principal. Adição dos componentes Contact e ContactItem. Incorporação destes últimos em Home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Contacts from '../contacts/Contacts'
+import ContactState from '../../context/contact/ContactState'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="grid-2">
+            <div></div>
+            <div>
+                <Contacts />
+            </div>
         </div>
     )
 }
